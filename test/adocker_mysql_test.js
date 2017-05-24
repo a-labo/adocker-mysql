db56b9620f6679d596af9dc579447f51e411f569
Taka Okunishi <okunishinishi@gmail.com>
2017-05-24 22:10:43 +0900

    Version incremented to 3.0.4

--- test/adocker_mysql_test.js
+++ test/adocker_mysql_test.js
@@ -5,7 +5,7 @@
 'use strict'
 
 const adockerMysql = require('../lib/adocker_mysql.js')
-const assert = require('assert')
+const { equal } = require('assert')
 const co = require('co')
 
 describe('adocker-mysql', function () {
@@ -22,9 +22,22 @@ describe('adocker-mysql', function () {
   it('Adocker mysql', () => co(function * () {
     let mysql = adockerMysql('adocker-mysql-test-01')
 
-    let { run, remove, logs } = mysql.cli()
+    let { run, remove, logs, stop, isRunning, hasBuild } = mysql.cli()
+
+    equal(yield isRunning(), false)
+    equal(yield hasBuild(), false)
     yield run()
+
+    equal(yield isRunning(), true)
+    equal(yield hasBuild(), true)
+
     yield logs()
+
+    yield stop()
+
+    equal(yield isRunning(), false)
+    equal(yield hasBuild(), true)
+
     yield remove({ force: true })
   }))
 })
